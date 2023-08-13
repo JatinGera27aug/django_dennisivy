383d7c338b5e614d6a969d13c3bb8ce5171213b9
Your Name <you@example.com>
2023-08-14 02:04:54 +0530

    do all

--- base/urls.py
+++ base/urls.py
@@ -6,6 +6,10 @@ from . import views
 #idhar ham saaman/request le rhe hain
 urlpatterns=[
     path('', views.home, name="home"),
-    path('room/', views.room, name="room") #pointing to views of base
+    path('room/<str:pk>', views.room, name="room") #pointing to views of base
 
 ]
+#why we have provided name var here: so that if sometime we changes our
+# template name from room to smthng else, we don't have to change url
+# in every folder, name=room will keep care of it
+# so no need to paas url , just put name 
--- base/views.py
+++ base/views.py
@@ -8,11 +8,24 @@ from django.shortcuts import render
 
 
 
+rooms=[
+    {'id':1, 'name':'Lets learn python'},
+    {'id':2, 'name':'Design'},
+    {'id':3, 'name':'DSA'}
+]
+
 def home(request): 
-    return render(request, 'home.html')
+    context = {'rooms':rooms}
+    return render(request, 'base/home.html', context)  #2nd rooms means what value we pass
+
     #return HttpResponse('HOME PAGE') 
 
 
-def room(request):
-    return render(request, 'room.html')
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id']==int(pk):
+            room = i
+    context = {'room':room}
+    return render(request, 'base/room.html',context)
    

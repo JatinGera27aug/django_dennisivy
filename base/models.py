f1bb106795b840d409634098b81dbdaf4ee4b6cc
Your Name <you@example.com>
2023-08-17 23:09:37 +0530

    moving ahead with models

--- base/models.py
+++ base/models.py
@@ -1,13 +1,25 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.db.models.deletion import CASCADE
 # Create your models here.
 
 # here we will create our database tables
 # for the users and posts
 
+
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
 class Room(models.Model):
-    #host = 
-    #topic = 
+    host =  models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
+    # (<topicname> when it is already made or upper in code
+    # else "topicname" to access anywhere)
+
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)  #2nd blan = True is for save mehtod, frist was for database null=True wla
 
@@ -16,5 +28,28 @@ class Room(models.Model):
 
     #participants = 
     updated = models.DateTimeField(auto_now=True)
-    created = models.DateTimeField(auto_now_add=True, editable=False)
+    created = models.DateTimeField(auto_now_add=True, editable=False)   #inital time stamp hi store rhegi
 
+    def __str__(self):
+        return self.name
+    
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # cascade means delete fully
+    # below command is used when a Room is deleted, its children will also be deleetd
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    #set_null will used to remain the existence of var
+    # on delete we do with one-to-many relations
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True, editable=False)   #inital time stamp hi store rhegi
+
+    def __str__(self):
+        return self.body[0:50]  #will return only first 50 letters
+    
+
+
+
+
+# for first running python manage.py makemigrations, we get 0001_initial.py in migrations older
+# it will keep on increasing
--- base/views.py
+++ base/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .models import Room
 # Create your views here.
 
 #there are gonna be what we need to render, contains whole data when someone 
@@ -8,13 +8,15 @@ from django.shortcuts import render
 
 
 
-rooms=[
-    {'id':1, 'name':'Lets learn python'},
-    {'id':2, 'name':'Design'},
-    {'id':3, 'name':'DSA'}
-]
+# rooms=[
+#     {'id':1, 'name':'Lets learn python'},
+#     {'id':2, 'name':'Design'},
+#     {'id':3, 'name':'DSA'}
+# ]
 
 def home(request): 
+    #below pointing to Room model name in model.py
+    rooms = Room.objects.all()    #gets all rooms from db
     context = {'rooms':rooms}
     return render(request, 'base/home.html', context)  #2nd rooms means what value we pass
 
@@ -22,10 +24,15 @@ def home(request):
 
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id']==int(pk):
-            room = i
+
+    room = Room.objects.get(id=pk)   #now they will show room along with correct links
+    #id in models are by default specified , starting from 1
+    #and incrementing by one
+    
+    # room = None
+    # for i in rooms:
+    #     if i['id']==int(pk):
+    #         room = i
     context = {'room':room}
     return render(request, 'base/room.html',context)
    
